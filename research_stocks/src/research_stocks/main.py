7b651ab22d079f79456901f5e0d5578150999922
Ovidiu Moldovan <moldovan.ovidiuv@gmail.com>
2025-07-29 11:29:59 +0300

    fixe uvicorn

--- research_stocks/src/research_stocks/main.py
+++ research_stocks/src/research_stocks/main.py
@@ -176,4 +176,4 @@ def on_shutdown() -> None:
 if __name__ == "__main__":
   import uvicorn
 
-  uvicorn.run("research_stocks.main:app", host="0.0.0.0", port=8000)
+  uvicorn.run(app, host="0.0.0.0", port=8000)
